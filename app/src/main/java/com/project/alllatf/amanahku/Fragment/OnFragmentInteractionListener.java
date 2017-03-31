64e6b9964c71ead0c65ad582762b388880529af8
muhammadsasmito <muhammad.sasmito.a.w@gmail.com>
2017-03-31 21:22:37 +0700

    Revert "Merge remote-tracking branch 'origin/sashme' into sashme"
    This reverts commit 0a28301d85816981289fa8b6e309198f31500e64.

--- app/src/main/java/com/project/alllatf/amanahku/Fragment/OnFragmentInteractionListener.java
+++
@@ -1,9 +0,0 @@
-package com.project.alllatf.amanahku.Fragment;
-
-/**
- * Created by fachrul on 3/27/17.
- */
-
-public interface OnFragmentInteractionListener {
-        void onFragmentInteraction(String TAG, Object data);
-}
